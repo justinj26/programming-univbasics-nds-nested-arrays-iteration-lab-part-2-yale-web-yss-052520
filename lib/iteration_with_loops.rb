7ceeef9bb99610295dfb0ebbbe343b2ee38a8d29
Justin James <justin.james@yale.edu>
2020-05-06 17:31:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_min_in_nested_arrays(src)
  new_array = []
  while count < src.length do
    inner_count = 0 
-   low_temp = 0 
+   low_temp = 1 / 0.0  
    while inner_count < src[count].length do
      if src[count][inner_count] <= low_temp
        low_temp = src[count][inner_count]
